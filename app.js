77922a6b85113da23e99b9021061cbaf413612ed
James <james.re@yahoo.com>
2020-02-20 17:54:15 -0800

    initial data crated

---
+++ app.js
@@ -0,0 +1,50 @@
+// LOAD ENV FILES
+require('dotenv').config();
+
+// Application Dependencies
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const pg = require("pg");
+
+console.log(process.env);
+// Database Client
+const Client = pg.Client;
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan("dev")); // http logging
+app.use(cors()); // enable CORS request
+
+// API Routes
+
+app.get("/data", async (req, res) => {
+  try {
+    const result = await client.query(`
+            SELECT
+                name,
+                genre,
+                img,
+                year,
+                rating,
+                fresh,
+                FROM CATS;
+                `);
+                // CHECK WHAT CATS MEANS FOR WHAT VAR 
+    console.log(result.rows);
+
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message || err
+    });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("server running on PORT", PORT);
+});
---
+++ data/data.js
@@ -0,0 +1,86 @@
+// CREATE TABLE cats (
+    //     id SERIAL PRIMARY KEY NOT NULL,
+//     name VARCHAR(256) NOT NULL,
+//     genre VARCHAR(256) NOT NULL,
+//     url VARCHAR(256) NOT NULL,
+//     year INTEGER NOT NULL,
+//     lives INTEGER NOT NULL,
+//     is_sidekick BOOLEAN NOT NULL
+// );
+
+// INSERT INTO cats (name, genre, url, year, lives, is_sidekick)
+// VALUES ('FEAR', 'jellicle', 'www.placekitten.com/300/300', 1987, 2, true);
+
+// 1 name
+// 2 genre
+// 3 IMG URL
+// 4 year
+// 5 rating
+// rottentomatoes > 30% true or false
+module.exports = [
+    {
+        name: 'FEAR',   
+        genre: 'Thriller',
+        img: 0,
+        year: '1996',
+        rating: 'R',
+        fresh: 'True',
+    //or rotten depends on 
+    },
+    {
+        name: 'Boogie Nights',   
+        genre: 'Drama',
+        img: 0,
+        year: '1997',
+        rating: 'R',
+        fresh: 'True',
+    },
+    {
+        name: 'Four Brothers',   
+        genre: 'Drama',
+        img: 0,
+        year: 'Action',
+        rating: 'R',
+        fresh: 'Rotten',
+    },
+    {
+        name: 'The Departed',   
+        genre: 'Drama',
+        img: 0,
+        year: '2006',
+        rating: 'R',
+        fresh: 'True',
+    },
+    {
+        name: 'Ted',   
+        genre: 'Comedy',
+        img: 0,
+        year: '2012',
+        rating: 'R',
+        fresh: 'True',
+    },
+    {
+        name: 'Boogie Nights',   
+        genre: 'Drama',
+        img: 0,
+        year: '1997',
+        rating: 'R',
+        fresh: 'True',
+    },
+    {
+        name: 'The Figher',   
+        genre: 'Drama',
+        img: 0,
+        year: '2010',
+        rating: 'R',
+        fresh: 'True',
+    },
+    {
+        name: 'Other Guys',   
+        genre: 'Comedy',
+        img: 0,
+        year: '2010',
+        rating: 'R',
+        fresh: 'True',
+    }];
+
