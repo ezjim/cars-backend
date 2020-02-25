7e0fcb730b504ed091a3397ea5f567d985f0aa8b
James <james.re@yahoo.com>
2020-02-25 10:55:49 -0800

    ok

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.post('/data', async (req, res) => {
 
         const result = await client.query(`
         INSERT INTO movies (name, type, img, year, rating, fresh)
-                    VALUES ($1, $2, $3, $4, $5, $6);
+                    VALUES ($1, $2, $3, $4, $5, $6)
                         RETURNING *; 
                         `,
             [req.body.name, req.body.type, req.body.img, req.body.year, req.body.rating, req.body.fresh]
